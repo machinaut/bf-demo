7e7cefc52ca7d0d28cd9ef5cd59badf66a4a6fae
Alex Ray <aray@google.com>
2013-07-27 21:38:45 -0700

    Initial commit

---
+++ bfi.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bf"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hellogod")
+	flag.Parse()
+	if (flag.NArg() < 1) {
+		fmt.Println("Needs file to run on command line.  Bugger off.")
+		return;
+	}
+	prog, _ := os.Open(flag.Arg(0))
+	bfi := bf.NewBFInterp(prog, os.Stdin, os.Stdout)
+	bfi.Run()
+}
+
